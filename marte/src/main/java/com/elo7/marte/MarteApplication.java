af7d0375294c59fd1e71571091e924b5a458ec60
Denis Schimidt <denis.schimidt@gmail.com>
2016-06-27 23:46:17 -0300

    Inclusão do Swagger e alteração de configs

--- marte/src/main/java/com/elo7/marte/MarteApplication.java
+++ marte/src/main/java/com/elo7/marte/MarteApplication.java
@@ -1,9 +1,18 @@
 package com.elo7.marte;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@SpringBootApplication(exclude={ DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, RepositoryRestMvcAutoConfiguration.class })
 @ComponentScan(basePackages = "com.elo7.marte")
+@EnableWebMvc
+@EnableJpaRepositories
 public class MarteApplication{
 
 	public static void main(String[] args) throws Exception {
--- marte/src/main/java/com/elo7/marte/application/util/AppConfig.java
+++ marte/src/main/java/com/elo7/marte/application/util/AppConfig.java
@@ -1,24 +1,15 @@
-package com.elo7.marte;
+package com.elo7.marte.application.util;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, RepositoryRestMvcAutoConfiguration.class })
-@EnableWebMvc
-@EnableJpaRepositories
 public class AppConfig extends WebMvcConfigurerAdapter{
     
 	@Bean(name = "jsonMessageConverter")
